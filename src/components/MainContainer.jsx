64b11ef7c714d10aed2596f017004c1d43f09894
Judith Arrizza <judith.arrizza@gmail.com>
2024-12-13 13:11:50 -0800

    more chocolate

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -3,9 +3,9 @@ import '../index.css'
 import { Canvas} from '@react-three/fiber'
 
 
-// import Chocolate from './canvas/ChocolateBowl';
-// import Chocolate from './canvas/ChocolateLeft';
-import Chocolate from './canvas/ChocolateSwirl';
+import Chocolate from './canvas/Chocolate';
+
+import { OrbitControls } from '@react-three/drei';
 
 const MainContainer = () => {
   const bgColor = ({ gl }) => {
@@ -18,9 +18,10 @@ const MainContainer = () => {
       style={{position: 'fixed'}}
       camera={{ position: [20, 3, 8], fov: 25}}
       onCreated={bgColor}>  
-      <pointLight intensity={2} color={0xff0000} position={[0, 5, 5]} />
-      <spotLight intensity={3} color={0xff0000} position={(-20, 50, 10)} />
-      <Chocolate />
+      <pointLight intensity={3} color={0xff0000} position={[0, 5, 5]} />
+      <spotLight intensity={4} color={0xff0000} position={(-20, 50, 10)} />
+      <OrbitControls />
+      <Chocolate name={'Swirl'} scale={0.08} />
     </Canvas>
   )
 }
---
+++ src/components/canvas/Chocolate.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useMemo } from 'react';
+import { useGLTF } from '@react-three/drei';
+
+export default function Chocolate({name, scale}) {
+     const model = useMemo(() => useGLTF(`./chocolate${name}/scene.gltf`), []);
+  return (
+    <mesh>
+        <primitive 
+            object={model.scene} 
+            rotation={[0, 8, 0]}
+            scale={scale} 
+        />
+    </mesh>
+  )
+}
